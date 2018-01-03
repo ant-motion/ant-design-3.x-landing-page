b0cfaa09d4a550d2d3174bd039022f5b5968adac
jljsj <liangjun.jiang@alipay.com>
2018-01-03 16:09:10 +0800

    update logo img

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -6,7 +6,7 @@ export default function Header(props) {
     <header {...props} id="header">
       <Link id="logo">
         <img alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
-        <img alt="Ant Design" src="https://gw.alipayobjects.com/zos/rmsportal/DmMsyewtkhAzjNkeGnYe.svg" />
+        <img alt="Ant Design" src="https://gw.alipayobjects.com/zos/rmsportal/DkKNubTaaVsKURhcVGkh.svg" />
       </Link>
     </header>
   );
